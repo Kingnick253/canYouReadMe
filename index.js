b32dc230d47f1edce35953573066a0eb218b7710
Kingnick253 <nicholaswebb2016@gmail.com>
2022-08-03 15:09:37 -0700

    Finished questions for prompt

--- index.js
+++ index.js
@@ -19,15 +19,53 @@ const markdown = require('./generateMarkdown.js');
 // 4. how to report issues.
 // 5. how to make contributions.
 // TODO: Create an array of questions for user input
-const questions = [
-    inquirer
+const questions = () => {
+    return inquirer
         .prompt([
             {
-                type:''
-            }
+                type:'input',
+                message: 'What is the title of your project?',
+                name: 'title',
+            },
+            {
+                type:'input',
+                message:'Please provide a brief description of your project:',
+                name:'Description',
+            },
+            {
+                type:'input',
+                message: 'Please provided the steps needed to install this application:',
+                name: 'installation',
+            },
+            {
+                type:'input',
+                message:'Please provide instructions and examples of use:',
+                name: 'usage',
+            },
+            {
+                type:'input',
+                message:'List all the collaborators if any:',
+                name:'credit',
+            },
+            {
+                type:'list',
+                message:'Please select which license you would like to use from this list:',
+                choice: ['Apache License 2.0', 'Boost Software License 1.0','BSD 3-License','BSD 2-License','Eclipse Pubic License 1.0', 'GNU GPL v3', 'GNU LGPL v3', 'GNU AGPL v3','The MIT License', 'The Unlicense'],
+                name: 'License',
+            },
+            {
+                type:'input',
+                message:'What is your github username?',
+                name:'github',
+            },
+            {
+                type:'input',
+                message:'What is your email?',
+                name:'email',
+            },
         ])
-];
-
+    }
+questions();
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
@@ -35,4 +73,4 @@ function writeToFile(fileName, data) {}
 function init() {}
 
 // Function call to initialize app
-init();
+// init();
