5f7aaac386e366b4105c2b49d16c081f6b8d1f62
Kingnick253 <nicholaswebb2016@gmail.com>
2022-08-09 17:10:55 -0700

    Added readme outline to the template literal in generateMarkdown.js file

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -20,9 +20,54 @@ function renderTitle(title) {
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${renderTitle(data.title)}
+    return `# ${renderTitle(data.title)}
 
-`;
+    ## Description
+    
+    Brief description explaining the what, why, and how
+    - what was the motivation?
+    - Why did you build this project?
+    - What problem does it solve?
+    - What did you learn?
+    
+    ## Table of Contents
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [License](#license)
+    - [Contributions](#contribution)
+    - [Tests](#tests)
+    - [Questions](#questions)
+    
+    ## Installation
+    
+    What are the steps required to install this
+    
+    ## Usage
+    
+    Provide instructions and examples for use. Include Screenshots/Screenrecords
+    
+    ## Credits
+    md
+    List of collaaborators if any link their Github profiles.
+    Any third-party assets that require attribution
+    
+    
+    ## License
+    
+    Add License information
+    
+    ## Contributions
+    
+    If you created an application of package and would like other development
+    
+    ## Tests
+    
+    This is where the test that you have made will go 
+    
+    ## Questions
+    
+    (have a user input that will add Github user name that will take them to github profile.)(when the users inputs email this will display to this section as well and give instructions on to contact user with more questions)
+     `;
 }
 
 module.exports = generateMarkdown;//grabs the whole function from this file that will make the README.md
